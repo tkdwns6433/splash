ad4afc10ae62c5391a9ae31a7c32ee4d25501246
root <tkdwns6433@naver.com>
2019-08-10 07:57:50 -0700

    get movie metadata from uploaded file

--- restServer.js
+++ restServer.js
@@ -166,3 +166,22 @@ var runEncodingServer = async function(){
 
 setInterval(runEncodingServer, 3000);
 
+app.get('/magnet', cors(corsOption), (req, res) => {
+	var Client = require('node-torrent');
+	var client = new Client({loglevel: 'DEBUG'});
+	var torrent = client.addTorrent('magnet:?xt=urn:btih:c9e15763f722f23e98a29decdfae341b98d53056&dn=Cosmos+Laundromat&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fcosmos-laundromat.torrent');
+	torrent.on('progress', function() {
+		console.log('downloading files : ' + torrent._files);
+		console.log('progress : ' + torrent.stats.downloaded / torrent.size + '%');
+	})
+	.on('complete', function(){
+		console.log('complete download');
+		torrent.files.forEach(function(file){
+			var newPath = 'vods/downloads/' + file.path;
+			fs.rename(file.path, newPath);
+			file.path = newPath;
+		});
+	});
+	res.status(200).send();
+});
+
